99753574b8dd009f50b1931c36949a00fafe6379
Lazaro Candea <lazaro@candea.es>
2025-09-10 21:45:04 +0200

    Better project structure

--- src/pybedca/__init__.py
+++ src/pybedca/__init__.py
@@ -5,6 +5,6 @@ This package provides a Python interface to interact with the BEDCA database.
 """
 
 from .client import BedcaClient
-from .models import FoodPreview
+from .models import FoodPreview, Food
 
-__all__ = ["BedcaClient", "FoodPreview"]
+__all__ = ["BedcaClient", "FoodPreview", "Food"]
